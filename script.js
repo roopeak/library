f344895641716a6fdb3780c94e3d6dee15a48cf3
roopeak <roopeak@protonmail.com>
2022-12-27 18:03:29 +0200

    Form implemention for asking books

--- script.js
+++ script.js
@@ -1,52 +1,78 @@
 let bookDisplay = document.querySelector(".book-display");
 
+const addBookBtn = document.querySelector(".add-book");
+const submitBtn = document.querySelector(".submit-button");
+
+addBookBtn.addEventListener("click", () => {
+    const addBookForm = document.querySelector(".add-book-form");
+    addBookForm.style.display = "flex";
+});
+
+submitBtn.addEventListener("click", () => {
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.querySelector('input[name="read"]:checked').value;
+
+    event.preventDefault();
+
+    addBookToLibrary(title, author, pages, read);
+    const addBookForm = document.querySelector(".add-book-form");
+    addBookForm.style.display = "none";
+
+    document.getElementById("title").value = "";  
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").value = "";  
+})
+
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
-
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    // this.info = function() {
-    //     return title, author, pages, read;
-    // };
-
 };
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
 
-    const book = new Book("The Hobbit", "J. R. R. Tolkien", "295", "False");
-    const book2 = new Book("Factfulness", "Hans Rosling", "378", "True");
-    myLibrary.push(book);
-    myLibrary.push(book2);
+    // let title = document.getElementById("title").value;
+    // let author = document.getElementById("author").value;
+    // let pages = document.getElementById("pages").value;
+    // let read = document.getElementById("read").value;
 
-    // book.info();
-    // book2.info();
-    // console.log(myLibrary[0])
+    // console.log(title, author, pages, read)
 
-    for (let i = 0; i < myLibrary.length; i++) {
-        let card = document.createElement("div");
-        bookDisplay.appendChild(card);
-        for (item in myLibrary[i]) {
+    // title = prompt("Give title: ");
+    // author = prompt("Give author: ");
+    // pages = parseInt(prompt("Give page numbers: "));
 
-            let div = document.createElement("div");
-            card.appendChild(div);
-            div.innerHTML += myLibrary[i][item];
-
-            console.log(myLibrary[i][item]);
-
-
-        }
-    }
-    // bookDisplay.appendChild(card);
+    // keepGoing = true;
+    // while (keepGoing) {
+    //     if (isNaN(pages) == true) {
+    //         pages = parseInt(prompt("Number of pages must be a number: "));
+    //     } else {
+    //         keepGoing = false;
+    //     }
+    // }
     
+    // read = prompt("Have you read it?");
 
+    const book = new Book(title, author, pages, read);
+
+    myLibrary.push(book);
+
+    let card = document.createElement("div");
+    bookDisplay.appendChild(card);
+
+    for (item in book) {
+        let div = document.createElement("div");
+        card.appendChild(div);
+        div.innerHTML += book[item];
+    }
 };
 
-
-
-addBookToLibrary();
+// addBookToLibrary();
 // console.log(myLibrary);
 
