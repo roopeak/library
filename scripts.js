8517ac50bcd61f52f1980dc7b4d5cb07da20c2ab
Roope Kärkkäinen <roopeak@protonmail.com>
2025-02-10 15:39:31 +0200

    Add a constructor to Book and a function to add a book to library

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
-console.log('Hello World!');
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+};
+
+function addBookToLibrary() {
+	const book = new Book('The Hobbit', 'J.R.R. Tolkien', 328, true);
+	myLibrary.push(book);
+	console.log(book);
+};
+
+addBookToLibrary();
