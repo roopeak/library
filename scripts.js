38ba02176d3093030c73489a3606590a73dc937f
Roope Kärkkäinen <roopeak@protonmail.com>
2025-02-14 18:46:13 +0200

    Create a book class

--- scripts.js
+++ scripts.js
@@ -189,3 +189,28 @@ function clearNewBookForm() {
 if (myLibrary.length > 0) {
 	generateCards();
 }
+
+class Book {
+	constructor(title, author, pages, read) {
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = read;
+	}
+
+	get title() {
+		return this._title;
+	}
+
+	get author() {
+		return this._author;
+	}
+
+	get pages() {
+		return this._pages;
+	}
+
+	get read() {
+		return this._read;
+	}
+}
